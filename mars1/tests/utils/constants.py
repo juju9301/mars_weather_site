09257ac88cff99f0a0bb92b1e2d444513ebe27df
Ivan Kovresov <b7official@gmail.com>
2026-01-14 19:46:26 +0100

    change app name to mars_weather_app

--- mars1/tests/utils/constants.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-BASE_URL = 'http://localhost:8000/'
-TEST_FILES = {'jpg': Path('media/test_images/test_image1.jpg'), 
-              'png': Path('media/test_images/test_image2.png')}
-WEATHER_FIXTURE_PATH = Path(r'weather/fixtures/weather_fixture.json')
-API_GET_USERS_URL = BASE_URL + 'api/users'
-API_POST_CREATE_URL = BASE_URL + 'api/posts/'
-API_POST_DELETE_URL = BASE_URL + 'api/posts/delete'
--- src/mars_weather_app/asgi.py
+++ src/mars_weather_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mars1 project.
+ASGI config for mars_weather_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mars1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mars_weather_app.settings')
 
 application = get_asgi_application()
